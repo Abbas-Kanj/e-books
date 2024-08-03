6bf6d9edd26684976f039ad54e9da98abf65e61c
Abbas <abbas_kanj@hotmail.com>
2024-08-03 23:32:52 +0300

    modified the global layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Drawer from "@/components/Drawer";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="cyberpunk">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="luxury">
+      <body className={inter.className}>
+        <Drawer children={children} />
+        <Footer />
+      </body>
     </html>
   );
 }
