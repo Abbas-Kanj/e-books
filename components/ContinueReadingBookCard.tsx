13ddb83462bf49afc610f520192d40106f5fb448
Abbas <abbas_kanj@hotmail.com>
2024-08-04 17:05:36 +0300

    <Frontend> Created a continue reading book card

---
+++ components/ContinueReadingBookCard.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import React from "react";
+import cover from "@/assets/Images/rdpd.jpg";
+
+type Props = {};
+
+function ContinueReadingBookCard({}: Props) {
+  return (
+    <div className="flex items-center gap-2 w-[540px]">
+      <Image src={cover} alt="Book-Cover" className="size-28 rounded-lg" />
+      <div className="flex flex-col text-primary text-start">
+        <h2 className="font-semibold">Book Name</h2>
+        <h3 className="font-light">Chapter</h3>
+        <p className="font-medium text-wrap">
+          Description: ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+          lorem ipsum loremipsum lorem ipsum lorem ipsum lorem
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ContinueReadingBookCard;
