c3574b90b45d12c186a8cbee576fca26d05b9141
Abbas <abbas_kanj@hotmail.com>
2024-08-04 16:59:22 +0300

    <Frontend> added links to navigate through pages

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -1,15 +1,29 @@
+"use client";
 import React from "react";
 import Navbar from "./Navbar";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  MdDashboard,
+  MdBookmark,
+  MdLogout,
+  MdBook,
+  MdSettings,
+  MdExplore,
+  MdKeyboardArrowRight,
+} from "react-icons/md";
+import { usePathname } from "next/navigation";
 
 type Props = {
   children: any;
 };
 
 function Drawer({ children }: Props) {
+  const pathname = usePathname();
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col items-center">
+      <div className="drawer-content flex flex-col items-center w-full">
         {
           <>
             <Navbar />
@@ -18,26 +32,76 @@ function Drawer({ children }: Props) {
         }
         <label
           htmlFor="my-drawer-2"
-          className="btn btn-primary drawer-button lg:hidden"
+          className="btn btn-base-content px-2 opacity-60 hover:opacity-100 lg:hidden fixed left-1 top-60"
         >
-          Open drawer
+          <MdKeyboardArrowRight className="size-6" />
         </label>
       </div>
-      <div className="drawer-side">
+      <div className="drawer-side z-50">
         <label
           htmlFor="my-drawer-2"
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+        <div className="menu bg-base-200 text-base-content min-h-full items-center w-30 px-4 py-8 flex flex-col justify-between">
           {/* Sidebar content here */}
-          <li>
-            <a>Sidebar Item 1</a>
-          </li>
-          <li>
-            <a>Sidebar Item 2</a>
-          </li>
-        </ul>
+          <Link href="/">
+            {/* <Image src={} alt="" className=""/> */}
+            <MdBook className="size-10" />
+          </Link>
+          <div className="flex flex-col gap-6">
+            <Link
+              href="/"
+              className={`${
+                pathname === "/"
+                  ? "border-b-2 border-solid border-primary w-8"
+                  : ""
+              }  hover:opacity-50 self-center py-2`}
+            >
+              <MdDashboard className="size-8" />
+            </Link>
+            <Link
+              href="/explore"
+              className={`${
+                pathname === "/explore"
+                  ? "border-b-2 border-solid border-primary w-8"
+                  : ""
+              }  hover:opacity-50 self-center py-2`}
+            >
+              <MdExplore className="size-8" />
+            </Link>
+            <Link
+              href="/bookmarks"
+              className={`${
+                pathname === "/bookmarks"
+                  ? "border-b-2 border-solid border-primary w-8"
+                  : ""
+              }  hover:opacity-50 self-center py-2`}
+            >
+              <MdBookmark className="size-8" />
+            </Link>
+            <Link
+              href="/profileSettings"
+              className={`${
+                pathname === "/profileSettings"
+                  ? "border-b-2 border-solid border-primary w-8"
+                  : ""
+              }  hover:opacity-50 self-center py-2`}
+            >
+              <MdSettings className="size-8" />
+            </Link>
+          </div>
+          <div className="flex flex-col gap-10 items-center">
+            {/* Avatar Online */}
+            <div className="avatar online">
+              <div className="w-14 rounded-full">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+            {/* logout */}
+            <MdLogout className="size-8 cursor-pointer hover:opacity-40" />
+          </div>
+        </div>
       </div>
     </div>
   );
