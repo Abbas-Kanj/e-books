506778ffbc0c2e98c21af61d3ce25468ba3006ed
Abbas <abbas_kanj@hotmail.com>
2024-08-05 21:26:53 +0300

    <Config> added remote patterns to test image urls

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images-na.ssl-images-amazon.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "images-na.ssl-images-amazon.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
