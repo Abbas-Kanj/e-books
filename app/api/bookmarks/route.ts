07eea98247aa291710014785de05ea382db8215a
Abbas <abbas_kanj@hotmail.com>
2024-08-08 17:25:41 +0300

    <Backend> created a post api route to add or remove bookmarks

---
+++ app/api/bookmarks/route.ts
@@ -0,0 +1,50 @@
+import connectDB from "@/config/database";
+import Book from "@/models/Book";
+import User from "@/models/User";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+// POST /api/bookmarks
+export const POST = async (req: {
+  json: () => PromiseLike<{ bookId: any }> | { bookId: any };
+}) => {
+  try {
+    await connectDB();
+
+    const { bookId } = await req.json();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    // Find user in database
+    const user = await User.findOne({ _id: userId });
+
+    // Check if book is bookmarked
+    let isBookmarked = user.bookmarks.includes(bookId);
+
+    let message;
+
+    if (isBookmarked) {
+      // If already bookmarked, remove it
+      user.bookmarks.pull(bookId);
+      message = "Bookmark removed successfully";
+      isBookmarked = false;
+    } else {
+      // If not bookmarked, add it
+      user.bookmarks.push(bookId);
+      message = "Bookmark added successfully";
+      isBookmarked = true;
+    }
+
+    await user.save();
+
+    return new Response(JSON.stringify({ message, isBookmarked, status: 200 }));
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
