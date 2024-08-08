9724945420a105f27e81a343e2bb228dc6a73bec
Abbas <abbas_kanj@hotmail.com>
2024-08-08 13:51:25 +0300

    <Frontend> added a useEffect to await fetching books, added a loading state

--- components/TrendingSection.tsx
+++ components/TrendingSection.tsx
@@ -1,29 +1,48 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import BookCard from "./BookCard";
+import { fetchBooks } from "@/utils/requests";
+import Spinner from "./Spinner";
 
 type Props = {};
 
-function TrendingSection({}: Props) {
+const TrendingSection = async (props: Props) => {
+  const [loading, setLoading] = useState(true);
+  const [recentBooks, setRecentBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchBooksApi = async () => {
+      try {
+        const books = await fetchBooks();
+
+        setRecentBooks(
+          books.sort(() => Math.random() - Math.random()).slice(0, 6)
+        );
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchBooksApi();
+  }, []);
+
   return (
     <section className="flex flex-col w-full gap-5">
       <h1 className="text-2xl font-semibold w-fit text-primary">
         Trending Books 🔥
       </h1>
       <div className="flex justify-around">
-        <BookCard />
-        <div className="rounded-full w-1 h-56 self-center bg-current"></div>
-        <BookCard />
-        <div className="rounded-full w-1 h-56 self-center bg-current"></div>
-        <BookCard />
-        <div className="rounded-full w-1 h-56 self-center bg-current"></div>
-        <BookCard />
-        <div className="rounded-full w-1 h-56 self-center bg-current"></div>
-        <BookCard />
-        <div className="rounded-full w-1 h-56 self-center bg-current"></div>
-        <BookCard />
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          recentBooks.map((book: Book) => (
+            <BookCard key={book._id} book={book} />
+          ))
+        )}
       </div>
     </section>
   );
-}
+};
 
 export default TrendingSection;
