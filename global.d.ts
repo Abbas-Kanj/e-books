7822175c04569bec6113ca11e5fa50a7ce0db6f4
Abbas <abbas_kanj@hotmail.com>
2024-08-08 13:50:39 +0300

    <Types> created a global interface for books

---
+++ global.d.ts
@@ -0,0 +1,12 @@
+interface Book {
+  _id: string;
+  title: string;
+  author: string;
+  isbn: string;
+  price: number;
+  category: string;
+  description: string;
+  publishedDate: Date;
+  imageUrl: string;
+  audioUrl: string;
+}
