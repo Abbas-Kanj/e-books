1e27785af68bb977ff9b661df5c7ffcaf7d9ca14
Abbas <abbas_kanj@hotmail.com>
2024-08-08 13:55:08 +0300

    <Frontend> modified homePage main structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <div>hello</div>;
-}
+import CategoriesSection from "@/components/CategoriesSection";
+import ContinueReadingSection from "@/components/ContinueReadingSection";
+import TrendingSection from "@/components/TrendingSection";
+import connectDB from "@/config/database";
+
+const HomePage = async () => {
+  await connectDB();
+  return (
+    <main className="flex flex-col w-full items-start text-center p-4 gap-4">
+      <h2 className="self-center">"Knowledge is power." - Francis Bacon</h2>
+      <TrendingSection />
+      <div className="flex gap-4">
+        <ContinueReadingSection />
+        <CategoriesSection />
+      </div>
+    </main>
+  );
+};
+
+export default HomePage;
