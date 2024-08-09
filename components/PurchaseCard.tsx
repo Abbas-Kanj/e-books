627987339046cd85b0a7150ccf33bc9417d04dbe
Abbas <abbas_kanj@hotmail.com>
2024-08-09 16:32:47 +0300

    <Frontend> added to cards and styled each

--- components/PurchaseCard.tsx
+++ components/PurchaseCard.tsx
@@ -4,12 +4,31 @@ type Props = {};
 
 const PurchaseCard = (props: Props) => {
   return (
-    <div className="card bg-base-300 h-fit w-96 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+    <div className="flex flex-col gap-2">
+      <div className="card bg-primary text-primary-content h-fit w-80 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">
+            Free! <s>$5.99</s>
+          </h2>
+          <p className="text-sm">Available for a limited time</p>
+          <div className="card-actions justify-end mt-2">
+            <button className="btn btn-secondary btn-wide font-bold">
+              Add to collection
+            </button>
+            <button className="btn btn-primary shadow-none btn-wide font-bold">
+              Start reading
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="card h-fit bg-primary text-primary-content w-80 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">
+            Only for <span className="text-green-500">$5.99</span>{" "}
+          </h2>
+          <div className="card-actions justify-end mt-3">
+            <button className="btn btn-secondary">Add to cart</button>
+          </div>
         </div>
       </div>
     </div>
