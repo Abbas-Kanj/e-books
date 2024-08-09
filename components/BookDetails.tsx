6260c66c42d4922fc19f273f834fbe912f40dc30
Abbas <abbas_kanj@hotmail.com>
2024-08-09 17:56:29 +0300

    <Frontend> added a view more functionality

--- components/BookDetails.tsx
+++ components/BookDetails.tsx
@@ -1,55 +1,47 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
-type Props = {};
+type Props = {
+  book: Book;
+};
 
-const BookDetails = (props: Props) => {
+const BookDetails = ({ book }: Props) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = book.detailedSummary;
+
+  if (!showFullDescription) {
+    description = description.substring(0, 320) + "...";
+  }
+
   return (
     <section className="flex flex-col gap-2 w-[600px]">
-      <h1>Rich Dad Poor Dad</h1>
-      <h4>Robert T. Kiyoski</h4>
-      <p>
+      <h1 className="text-5xl font-bold">Rich Dad Poor Dad</h1>
+      <h4 className="text-sm italic">Robert T. Kiyoski</h4>
+      <p className="text-primary text-pretty">
         Rich Dad Poor Dad is a personal finance book written by Robert T. 1
         Kiyosaki and Sharon Lechter. 2 Published in 1997, it has become a global
         bestseller, translated into dozens of languages. 3 The book challenges
         conventional wisdom about money and investing, offering practical advice
         on building wealth and financial independence
       </p>
-      <h3>PUBLISHER DESCRIPTION</h3>
-      <p>
-        Rich Dad Poor Dad is a personal finance guide that uses a storytelling
-        approach to explain complex financial concepts. The book revolves around
-        the lives of the author, Robert Kiyosaki, and his two father figures:
-        his biological father (poor dad) and the father of his best friend (rich
-        dad). 1. Rich Dad Poor Dad: What the Rich Teach Their Kids About Money
-        That the Poor and Middle Class Do Not!: Kiyosaki, Robert T.:
-        9781612681139 - Amazon www.amazon.com 2. Rich Dad Poor Dad: What the
-        Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!
-        www.amazon.com The book contrasts the financial philosophies of these
-        two men. The "poor dad," despite being highly educated and holding a
-        stable job, emphasizes the importance of education and job security. The
-        "rich dad," on the other hand, focuses on financial education, asset
-        acquisition, and building multiple income streams. 1. Rich Dad Poor Dad
-        Summary: Key Takeaways & Review - ClickUp clickup.com Kiyosaki argues
-        that traditional education often fails to prepare people for financial
-        success. He emphasizes the importance of understanding the difference
-        between assets and liabilities, building cash flow, and developing
-        financial intelligence. The book covers topics such as: 1. Difference
-        Between Assets and Liabilities According to Robert Kiyosaki - WIC Europe
-        wiceurope.com Mindset shift: Challenging conventional beliefs about
-        money and work. 1. Rich Dad, Poor Dad Summary of Key Ideas and Review |
-        Robert T. Kiyosaki - Blinkist www.blinkist.com Financial education: The
-        importance of learning about investing and money management. 1. Rich Dad
-        Poor Dad Summary: Key Takeaways & Review - ClickUp clickup.com Asset
-        acquisition: Focusing on acquiring assets that generate income. 1. Rich
-        Dad Poor Dad | Book Summary - Playtext.app playtext.app Overcoming fear:
-        Addressing the psychological barriers to wealth building.
-        Entrepreneurship: The potential of starting your own business. Through
-        engaging storytelling and practical advice, Rich Dad Poor Dad aims to
-        inspire readers to take control of their financial futures and achieve
-        financial freedom. 1. Rich Dad Poor Dad - Wikipedia en.wikipedia.org
+      <h3 className="text-xl font-semibold">PUBLISHER DESCRIPTION</h3>
+      <p className="text-primary text-pretty">
+        {description}{" "}
+        <button
+          className="text-base-content text-sm"
+          onClick={() => setShowFullDescription((prevState) => !prevState)}
+        >
+          {showFullDescription ? "Show less" : "Show more"}
+        </button>
       </p>
-      <h4>Release Date: April 22nd 2021</h4>
-      <h4>Length: 308 pages</h4>
+
+      <h4 className="text-sm">
+        Release Date: <span className="text-primary"> April 22nd 2021</span>
+      </h4>
+      <h4 className="text-sm">
+        Length: <span className="text-primary"> 300 pages</span>
+      </h4>
     </section>
   );
 };
