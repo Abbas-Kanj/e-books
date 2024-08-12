9363e28117b7d5107ceec3bde0eb5771ebba6e40
Abbas <abbas_kanj@hotmail.com>
2024-08-12 21:23:51 +0300

    <Backend> created a route to create a review and a rating

---
+++ app/api/reviews/route.ts
@@ -0,0 +1,69 @@
+import connectDB from "@/config/database";
+import Book from "@/models/Book";
+import User from "@/models/User";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+// POST /api/reviews
+export const POST = async (req) => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    // Find user in database
+    const user = await User.findOne({ _id: userId });
+
+    const { bookId, rating, text } = await req.json();
+
+    // Find book in database
+    const book = await Book.findById(bookId);
+
+    const review = {
+      userId: userId,
+      rating: rating,
+      text: text,
+    };
+
+    // Update average rating
+    // reduce() reduces an array to a single value
+    const totalRatings = book.reviews.reduce(
+      (sum, review) => sum + review.rating,
+      0
+    );
+
+    // Update book data
+    const updatedBook = await Book.findByIdAndUpdate(
+      bookId,
+      {
+        $push: { reviews: review },
+        $set: { averageRating: totalRatings / book.reviews.length },
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    // Update user data
+    await User.findByIdAndUpdate(userId, {
+      $push: {
+        reviews: updatedBook.reviews[updatedBook.reviews.length - 1]._id,
+      },
+    });
+
+    return new Response(
+      JSON.stringify({ message: "Review added successfully", status: 201 })
+    );
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({ message: "Something went wrong", status: 500 })
+    );
+  }
+};
