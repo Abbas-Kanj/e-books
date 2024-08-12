957ba6b0117cb742b9a0489c0d52194b3725bf6b
Abbas <abbas_kanj@hotmail.com>
2024-08-12 14:00:00 +0300

    <Backend> created a route to GET books bu athour name

---
+++ app/api/books/author/[author]/route.ts
@@ -0,0 +1,27 @@
+import connectDB from "@/config/database";
+import Book from "@/models/Book";
+
+// GET /api/books/author
+export const GET = async (req, { params }) => {
+  try {
+    await connectDB();
+
+    const { author } = params;
+
+    const authourBooks = await Book.find({ author: author })
+      .limit(6)
+      .select("title author price isFree imageUrl")
+      // lean() is used for better performance as it returns plain JavaScript
+      // objects instead of full Mongoose documents.
+      .lean();
+
+    if (authourBooks.length === 0 || authourBooks.length === 1) {
+      return new Response(JSON.stringify([]), { status: 200 });
+    }
+
+    return new Response(JSON.stringify(authourBooks), { status: 200 });
+  } catch (error) {
+    console.error("Error fetching author books", error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
