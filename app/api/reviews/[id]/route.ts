cf63ab0aec770a33c32faf5cb8290f6084414c09
Abbas <abbas_kanj@hotmail.com>
2024-08-12 22:13:54 +0300

    <Backend> created a route to fetch reviews of a book

---
+++ app/api/reviews/[id]/route.ts
@@ -0,0 +1,23 @@
+import connectDB from "@/config/database";
+import Book from "@/models/Book";
+
+// GET /api/reviews/:id
+export const GET = async (req, { params }) => {
+  try {
+    await connectDB();
+
+    const { id } = params;
+
+    const reviews = await Book.findById(id).select("reviews -_id").populate({
+      path: "reviews.userId",
+      select: "username image -_id", // exclude the id
+    });
+
+    if (reviews.length === 0)
+      return new Response("No reviews found", { status: 404 });
+
+    return new Response(JSON.stringify({ reviews }), { status: 200 });
+  } catch (error) {
+    console.error(error);
+  }
+};
