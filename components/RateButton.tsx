fa46e77c1a9d4b3fb42499c794e609bf80459c71
Abbas <abbas_kanj@hotmail.com>
2024-08-12 21:25:03 +0300

    <Frontend> added onchange and value attributes

--- components/RateButton.tsx
+++ components/RateButton.tsx
@@ -1,35 +1,48 @@
-import React from "react";
+"use client";
+import React, { Dispatch, SetStateAction } from "react";
 
-type Props = {};
+type Props = {
+  setRating: Dispatch<SetStateAction<number>>;
+};
 
-const RateButton = (props: Props) => {
+const RateButton = ({ setRating }: Props) => {
   return (
     <div className="rating rating-md">
       <input
         type="radio"
         name="rating-7"
         className="mask mask-star-2 bg-orange-400"
+        value={1}
+        onChange={(e) => setRating(Number(e.target.value))}
         defaultChecked
       />
       <input
         type="radio"
         name="rating-7"
         className="mask mask-star-2 bg-orange-400"
+        value={2}
+        onChange={(e) => setRating(Number(e.target.value))}
       />
       <input
         type="radio"
         name="rating-7"
         className="mask mask-star-2 bg-orange-400"
+        value={3}
+        onChange={(e) => setRating(Number(e.target.value))}
       />
       <input
         type="radio"
         name="rating-7"
         className="mask mask-star-2 bg-orange-400"
+        value={4}
+        onChange={(e) => setRating(Number(e.target.value))}
       />
       <input
         type="radio"
         name="rating-7"
         className="mask mask-star-2 bg-orange-400"
+        value={5}
+        onChange={(e) => setRating(Number(e.target.value))}
       />
     </div>
   );
