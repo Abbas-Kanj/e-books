bcd65084066b2d61bfb50bb3ebd0c63197f35afa
Abbas <abbas_kanj@hotmail.com>
2024-08-14 13:56:10 +0300

    <Frontend> created a fetch request to get averageRating

--- components/Progress.tsx
+++ components/Progress.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+"use client";
+import { apiDomain } from "@/utils/requests";
+import { useParams } from "next/navigation";
+import React, { useEffect } from "react";
 
 type Props = {};
 
 const Progress = (props: Props) => {
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchAverageRating = async () => {
+      try {
+        const res = await fetch(`${apiDomain}/books/average/${id}`);
+
+        if (res.ok) {
+          const data = await res.json();
+          console.log(data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchAverageRating();
+  }, [id]);
+
   return (
     <div className="flex flex-col items-center p-1 gap-6">
       <progress className="progress w-96" value={0} max="100"></progress>
