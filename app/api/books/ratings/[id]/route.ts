ea12b1f25513c5c84e528eb333e01a81f0fd440a
Abbas <abbas_kanj@hotmail.com>
2024-08-14 19:40:09 +0300

    <Backend> removed the fetching of averageRating following the single responsibilty rule

--- app/api/books/ratings/[id]/route.ts
+++ app/api/books/ratings/[id]/route.ts
@@ -1,14 +1,12 @@
 import connectDB from "@/config/database";
 import Book from "@/models/Book";
 
-// GET /api/books/average/:id
+// GET /api/books/ratings/:id
 export const GET = async (req, { params }) => {
   try {
     await connectDB();
 
     const { id } = params;
-
-    const averageRating = await Book.findById(id).select("averageRating -_id");
 
     const book = await Book.findById(id).select("reviews.rating");
 
@@ -16,7 +14,7 @@ export const GET = async (req, { params }) => {
       (review: { rating: number }) => review.rating
     );
 
-    return new Response(JSON.stringify({ averageRating, ratings }), {
+    return new Response(JSON.stringify({ ratings }), {
       status: 200,
     });
   } catch (error) {
