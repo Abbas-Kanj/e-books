d2830f35c711a6d28030bfbfa51cf15ccedfcf12
Abbas <abbas_kanj@hotmail.com>
2024-08-15 12:10:18 +0300

    <Frontend> created a function to clear search results after navigating, created a loading state, handled 404 responses

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -2,15 +2,24 @@
 import { apiDomain } from "@/utils/requests";
 import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import SearchDropDown from "./SearchDropDown";
 
 const SearchBox = () => {
   // States
   const [searchField, setSearchField] = useState("");
+  const [searchResults, setSearchResults] = useState("");
+  const [loading, setLoading] = useState(false);
 
+  // Clear search field after navigating
+  const clearSearchField = () => {
+    setSearchField("");
+    setSearchResults("");
+  };
   useEffect(() => {
+    setLoading(true);
     const debounceTimer = setTimeout(async () => {
       if (searchField === "") {
-        toast.warning("Search field cannot be empty!");
+        setSearchResults("");
         return;
       }
       try {
@@ -23,39 +32,61 @@ const SearchBox = () => {
             searchedData: searchField.split(" "),
           }),
         });
+
         if (res.ok) {
           const data = await res.json();
-          console.log(data);
+          setLoading(false);
+          setSearchResults(data);
+        }
+
+        if (res.status === 404) {
+          setLoading(false);
+          setSearchResults("We couldn't find what you are looking for");
         }
       } catch (error) {
         console.error(error);
         toast.error("Cannot search at this time");
       }
-    }, 3000);
+    }, 2000);
     return () => clearTimeout(debounceTimer);
   }, [searchField]);
 
   return (
-    <label className="input input-bordered flex items-center gap-2">
-      <input
-        type="text"
-        className="grow w-80"
-        placeholder="Search book titles, authors, publishers..."
-        onChange={(e) => setSearchField(e.target.value)}
-      />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 16 16"
-        fill="currentColor"
-        className="h-4 w-4 opacity-70"
-      >
-        <path
-          fillRule="evenodd"
-          d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-          clipRule="evenodd"
+    <>
+      {searchField !== "" && (
+        <SearchDropDown
+          searchResults={searchResults}
+          clearSearchField={clearSearchField}
+          loading={loading}
         />
-      </svg>
-    </label>
+      )}
+
+      <label className="input input-bordered flex items-center gap-2">
+        <input
+          role="button"
+          type="text"
+          className="grow w-80"
+          placeholder="Search book titles, authors..."
+          onChange={(e) => {
+            setSearchField(e.target.value);
+            setSearchResults("");
+          }}
+          value={searchField}
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          className="h-4 w-4 opacity-70"
+        >
+          <path
+            fillRule="evenodd"
+            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </label>
+    </>
   );
 };
 
