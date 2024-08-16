471c2887ea34f213c8e4e96484750a68e8129455
Abbas <abbas_kanj@hotmail.com>
2024-08-16 21:36:10 +0300

    <Frontend> restyled texts, added next link for navigation

--- components/GenreSection.tsx
+++ components/GenreSection.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import BookCard from "./BookCard";
 import { fetchBooksByGenre } from "@/utils/requests";
+import Link from "next/link";
 
 type Props = {
   genre: string;
@@ -11,12 +12,18 @@ const GenreSection = async ({ genre }: Props) => {
 
   return (
     <div className="flex flex-col gap-5 items-start">
-      <h1 className="text-xl">{genre}</h1>
+      <h1 className="text-xl font-bold">{genre}</h1>
       <div className="flex gap-10 w-full">
         {books.map((book: any) => (
           <BookCard key={book._id} book={book} />
         ))}
       </div>
+      <Link
+        href={`/explore/${genre}`}
+        className="text-secondary-content self-end font-semibold hover:opacity-70 cursor-pointer"
+      >
+        More {genre}...
+      </Link>
     </div>
   );
 };
