47c8a48a1add9fa07f65d924aa02a4dddd2e6c36
Abbas <abbas_kanj@hotmail.com>
2024-08-16 21:13:31 +0300

    <Frontend> modified the component to be a server component

--- components/MoreFromAuthour.tsx
+++ components/MoreFromAuthour.tsx
@@ -1,43 +1,25 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import BookCard from "./BookCard";
-import Spinner from "./Spinner";
 import { fetchBooksByAuthor } from "@/utils/requests";
 
 type Props = {
   book: Book;
 };
 
-const MoreFromAuthour = ({ book }: Props) => {
+const MoreFromAuthour = async ({ book }: Props) => {
   const { author } = book;
-  const [loading, setLoading] = useState(true);
-  const [booksByAuthor, setBooksByAuthor] = useState([]);
-
-  useEffect(() => {
-    const fetchBooksByAuthorData = async (author: string) => {
-      try {
-        const res = await fetchBooksByAuthor(author);
-        setBooksByAuthor(res);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchBooksByAuthorData(author);
-  }, [author]);
+  const booksByAuthor = await fetchBooksByAuthor(author);
 
   return (
     <section className="container flex flex-col gap-3 p-4 min-h-56">
-      <h1 className="text-2xl font-semibold mb-4">
+      <h1 className="text-2xl font-semibold mb-4 text-primary">
         More from <span className="italic font-light">{author}</span>
       </h1>
-      {loading && <Spinner loading={loading} />}
-      {!loading && booksByAuthor.length === 0 ? (
-        <h2 className="text-center">No books available at this time</h2>
-      ) : (
-        <div className="flex justify-between px-2 w-full">
-          {booksByAuthor.map((book: Book, index: number) => (
+      <div className="flex gap-10 px-2 w-full">
+        {booksByAuthor.length === 0 ? (
+          <h2 className="mx-auto">No books available at this time</h2>
+        ) : (
+          booksByAuthor.map((book: Book, index: number) => (
             <div key={book._id} className="flex items-center">
               <BookCard book={book} />
               {/* Rendering seperators */}
@@ -45,9 +27,9 @@ const MoreFromAuthour = ({ book }: Props) => {
                 <div className="rounded-full w-1 h-56 self-center bg-current ml-8"></div>
               )}
             </div>
-          ))}
-        </div>
-      )}
+          ))
+        )}
+      </div>
     </section>
   );
 };
