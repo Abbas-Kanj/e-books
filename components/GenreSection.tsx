bfbcdf15104fadb00c9a5db7856c1dbb2c4bca21
Abbas <abbas_kanj@hotmail.com>
2024-08-16 17:41:20 +0300

    <Frontend> created a genre section component

---
+++ components/GenreSection.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import Spinner from "./Spinner";
+import BookCard from "./BookCard";
+import { fetchBooksByGenre } from "@/utils/requests";
+
+type Props = {
+  genre: string;
+};
+
+const GenreSection = async ({ genre }: Props) => {
+  const books = await fetchBooksByGenre(genre);
+
+  return (
+    <div className="flex flex-col gap-5 items-start">
+      <h1 className="text-xl">{genre}</h1>
+      {!books ? (
+        <Spinner loading={true} />
+      ) : (
+        <div className="flex gap-10 w-full">
+          {books.map((book: any) => (
+            <BookCard key={book._id} book={book} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default GenreSection;
