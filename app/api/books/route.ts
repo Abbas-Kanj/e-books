ad6342e9977bc86f3dcb4e9afe24961c4067b13a
Abbas <abbas_kanj@hotmail.com>
2024-08-17 12:59:07 +0300

    <Backend> removed commented code

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -1,17 +1,11 @@
 import connectDB from "@/config/database";
 import Book from "@/models/Book";
-import { getSessionUser } from "@/utils/getSessionUser";
 
 // GET /api/books
 export const GET = async () => {
   try {
     await connectDB();
 
-    // const sessionUser = await getSessionUser();
-
-    // if (!sessionUser || !sessionUser.userId) {
-    //   return new Response("User ID is required", { status: 401 });
-    // }
     const books = await Book.find({});
 
     return new Response(JSON.stringify(books), { status: 200 });
