4b59c84dc8d9a2e47c01c3e31e1ab7ecde271c47
Abbas <abbas_kanj@hotmail.com>
2024-08-25 20:47:55 +0300

    <Frontend> added an if statement to check window is defined before rendering

--- app/(main)/bookmarks/page.tsx
+++ app/(main)/bookmarks/page.tsx
@@ -3,27 +3,14 @@ import PdfReader from "@/components/PdfReader";
 import React from "react";
 import { pdfjs } from "react-pdf";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url
-).toString();
+if (typeof window !== "undefined") {
+  pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    "pdfjs-dist/build/pdf.worker.min.mjs",
+    import.meta.url
+  ).toString();
+}
 
 const BookmarksPage = () => {
-  // useEffect(() => {
-  //   const fetchUserBookmarks = async () => {
-  //     try {
-  //       const res = await fetch(`${apiDomain}/bookmarks`);
-  //       if (res.ok) {
-  //         const data = await res.json();
-  //         console.log(data.bookmarks);
-  //       }
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-  //   fetchUserBookmarks();
-  // }, []);
-
   return (
     <div>
       <PdfReader />
