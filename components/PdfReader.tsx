0322c42a04388b0459d3a707720313ed703ba53e
Abbas <abbas_kanj@hotmail.com>
2024-08-25 20:45:42 +0300

    <Frontend> fixed error window not defined on build time

--- components/PdfReader.tsx
+++ components/PdfReader.tsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+"use client";
+import { useState, useEffect } from "react";
 import { Document, Page } from "react-pdf";
 
 const PdfReader = () => {
   const pdfUrl = "/mdlb.pdf";
   const [numPages, setNumPages] = useState<number>();
   const [pageNumber, setPageNumber] = useState<number>(1);
+  const [pageWidth, setPageWidth] = useState<number>(600); // Default to 600 for SSR
+
+  useEffect(() => {
+    // Ensure this runs only on the client-side
+    if (typeof window !== "undefined") {
+      const width = window.innerWidth < 640 ? window.innerWidth * 0.9 : 600;
+      setPageWidth(width);
+    }
+  }, []);
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
     setNumPages(numPages);
@@ -21,7 +31,7 @@ const PdfReader = () => {
           pageNumber={pageNumber}
           renderTextLayer={false}
           renderAnnotationLayer={false}
-          width={window.innerWidth < 640 ? window.innerWidth * 0.9 : 600}
+          width={pageWidth}
         />
       </Document>
       <div className="text-center mt-4">
