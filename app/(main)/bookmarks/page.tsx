f5beba743a513bdcc4db1d100fceca8673b00c21
Abbas <abbas_kanj@hotmail.com>
2024-08-25 20:56:26 +0300

    <Frontend> added useEffect to check for window on page load

--- app/(main)/bookmarks/page.tsx
+++ app/(main)/bookmarks/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 import PdfReader from "@/components/PdfReader";
-import React from "react";
+import React, { useEffect } from "react";
 import { pdfjs } from "react-pdf";
 
-if (typeof window !== "undefined") {
-  pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    "pdfjs-dist/build/pdf.worker.min.mjs",
-    import.meta.url
-  ).toString();
-}
-
 const BookmarksPage = () => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+        "pdfjs-dist/build/pdf.worker.min.mjs",
+        import.meta.url
+      ).toString();
+    }
+  }, []); // Runs once on mount
   return (
     <div>
       <PdfReader />
