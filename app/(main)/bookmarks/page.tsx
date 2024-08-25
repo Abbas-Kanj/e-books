d83e93bfc278e0566a6875128094195b2ccbb166
Abbas <abbas_kanj@hotmail.com>
2024-08-25 20:23:19 +0300

    <Frontend> modified bookmarks page to test pdf reader component

--- app/(main)/bookmarks/page.tsx
+++ app/(main)/bookmarks/page.tsx
@@ -1,26 +1,34 @@
 "use client";
-import { apiDomain } from "@/utils/requests";
-import React, { useEffect } from "react";
+import PdfReader from "@/components/PdfReader";
+import React from "react";
+import { pdfjs } from "react-pdf";
 
-type Props = {};
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
 
-function BookmarksPage({}: Props) {
-  useEffect(() => {
-    const fetchUserBookmarks = async () => {
-      try {
-        const res = await fetch(`${apiDomain}/bookmarks`);
-        if (res.ok) {
-          const data = await res.json();
-          console.log(data.bookmarks);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchUserBookmarks();
-  }, []);
+const BookmarksPage = () => {
+  // useEffect(() => {
+  //   const fetchUserBookmarks = async () => {
+  //     try {
+  //       const res = await fetch(`${apiDomain}/bookmarks`);
+  //       if (res.ok) {
+  //         const data = await res.json();
+  //         console.log(data.bookmarks);
+  //       }
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   fetchUserBookmarks();
+  // }, []);
 
-  return <div>BookmarksPage</div>;
-}
+  return (
+    <div>
+      <PdfReader />
+    </div>
+  );
+};
 
 export default BookmarksPage;
