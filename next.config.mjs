88fb8f6c9990e9a0fa496eb5e60ccfc63e09ad59
Abbas <abbas_kanj@hotmail.com>
2024-08-25 20:48:41 +0300

    <Nextconfig> modified webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
+  webpack: (config, { isServer }) => {
+    config.resolve.extensions.push(".mjs");
+
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
 
     return config;
   },
