bc4eb9119a6da6d099701c314eba665cb335df05
Abbas <abbas_kanj@hotmail.com>
2024-08-27 20:25:01 +0300

    <Frontend> installed a previous version of node to test pdf reader

--- components/PdfReader.tsx
+++ components/PdfReader.tsx
@@ -22,25 +22,25 @@ const PdfReader = ({ pdfFileName }: Props) => {
   const [loading, setLoading] = useState(false);
   const [numPages, setNumPages] = useState<number>();
   const [pageNumber, setPageNumber] = useState<number>(1);
-  const [pdfUrl, setPdfUrl] = useState<string | null>();
+  const [pdfUrl, setPdfUrl] = useState<string | null>(null);
 
-  // useEffect(() => {
-  //   const fetchPdf = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         `${apiDomain}/download?fileName=${encodeURIComponent(pdfFileName)}`
-  //       );
-  //       const blob = await response.blob();
-  //       const url = window.URL.createObjectURL(blob);
-  //       setPdfUrl(url);
-  //     } catch (error) {
-  //       console.error(error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-  //   fetchPdf();
-  // }, [pdfFileName]);
+  useEffect(() => {
+    const fetchPdf = async () => {
+      try {
+        const response = await fetch(
+          `${apiDomain}/download?fileName=${encodeURIComponent(pdfFileName)}`
+        );
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        setPdfUrl(url);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPdf();
+  }, [pdfFileName]);
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
     setNumPages(numPages);
@@ -53,56 +53,56 @@ const PdfReader = ({ pdfFileName }: Props) => {
   return (
     <>
       {!pdfUrl && loading && <Spinner loading={loading} />}
-      {/* {pdfUrl && !loading && ( */}
-      <div className="flex flex-col gap-8">
-        <Document
-          options={options}
-          file="/mdlb.pdf"
-          onLoadSuccess={onDocumentLoadSuccess}
-          error={
-            <div className="flex flex-col gap-5">
-              <h1 className="text-2xl mx-auto mt-10 text-error">
-                Error loading pdf 😕
-              </h1>
+      {pdfUrl && !loading && (
+        <div className="flex flex-col gap-8">
+          <Document
+            options={options}
+            file={pdfUrl}
+            onLoadSuccess={onDocumentLoadSuccess}
+            error={
+              <div className="flex flex-col gap-5">
+                <h1 className="text-2xl mx-auto mt-10 text-error">
+                  Error loading pdf 😕
+                </h1>
+                <button
+                  onClick={() => router.refresh()}
+                  className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg p-1"
+                >
+                  Try again
+                </button>
+              </div>
+            }
+          >
+            <Page
+              pageNumber={pageNumber}
+              renderTextLayer={false}
+              renderAnnotationLayer={false}
+              width={window.innerWidth < 640 ? 400 : 700}
+            />
+          </Document>
+          {numPages && (
+            <div className="join self-center">
               <button
-                onClick={() => router.refresh()}
-                className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg p-1"
+                className="join-item btn"
+                onClick={() => handlePageChange(-1)}
+                disabled={pageNumber === 1}
               >
-                Try again
+                «
+              </button>
+              <p className="join-item btn">
+                Page {pageNumber} of {numPages}
+              </p>
+              <button
+                className="join-item btn"
+                onClick={() => handlePageChange(1)}
+                disabled={pageNumber === numPages}
+              >
+                »
               </button>
             </div>
-          }
-        >
-          <Page
-            pageNumber={pageNumber}
-            renderTextLayer={false}
-            renderAnnotationLayer={false}
-            width={window.innerWidth < 640 ? 400 : 700}
-          />
-        </Document>
-        {numPages && (
-          <div className="join self-center">
-            <button
-              className="join-item btn"
-              onClick={() => handlePageChange(-1)}
-              disabled={pageNumber === 1}
-            >
-              «
-            </button>
-            <p className="join-item btn">
-              Page {pageNumber} of {numPages}
-            </p>
-            <button
-              className="join-item btn"
-              onClick={() => handlePageChange(1)}
-              disabled={pageNumber === numPages}
-            >
-              »
-            </button>
-          </div>
-        )}
-      </div>
-      {/* )} */}
+          )}
+        </div>
+      )}
     </>
   );
 };
