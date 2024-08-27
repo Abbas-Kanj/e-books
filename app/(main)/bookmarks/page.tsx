ea06038ec8420d4dafe81dbf44997ea5ed229081
Abbas <abbas_kanj@hotmail.com>
2024-08-27 16:07:52 +0300

    <Frontend> removed pdf reader component

--- app/(main)/bookmarks/page.tsx
+++ app/(main)/bookmarks/page.tsx
@@ -1,15 +1,10 @@
-import PdfReader from "@/components/PdfReader";
 import React from "react";
 import { polyfillPromiseWithResolvers } from "@/utils/polyfilsResolver";
 import "core-js/full/promise/with-resolvers.js";
 polyfillPromiseWithResolvers();
 
 const BookmarksPage = () => {
-  return (
-    <div>
-      <PdfReader />
-    </div>
-  );
+  return <div>BookmarksPage</div>;
 };
 
 export default BookmarksPage;
