b059ceb1ca04ff36129ff7b984ce3bc3575a411e
Abbas <abbas_kanj@hotmail.com>
2024-08-28 19:06:04 +0300

    <Frontend> restyling for better responsivenses

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,13 +37,13 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-100 mb-5">
       <div className="navbar-start">
-        <a className="md:ml-6 md:text-xl text-lg text-primary">Bookly</a>
+        <a className="md:ml-6 md:text-xl text-sm text-primary">Bookly</a>
       </div>
       <div className="navbar-center">
         <SearchBox />
       </div>
       <div className="md:hidden navbar-end">
-        <label className="btn btn-circle swap swap-rotate">
+        <label className="btn btn-circle btn-sm swap swap-rotate">
           {/* this hidden checkbox controls the state */}
           <input
             type="checkbox"
@@ -52,10 +52,8 @@ const Navbar = () => {
 
           {/* hamburger icon */}
           <svg
-            className="swap-off fill-current"
+            className="swap-off fill-current size-6"
             xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
             viewBox="0 0 512 512"
           >
             <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
@@ -63,10 +61,8 @@ const Navbar = () => {
 
           {/* close icon */}
           <svg
-            className="swap-on fill-current"
+            className="swap-on fill-current size-6"
             xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
             viewBox="0 0 512 512"
           >
             <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
