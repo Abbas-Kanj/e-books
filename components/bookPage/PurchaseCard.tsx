f43312cfa26508236f6cf54bb4576f28489b0e46
Abbas <abbas_kanj@hotmail.com>
2024-08-28 16:36:29 +0300

    <Frontend> added an renderer option to check if is free

--- components/bookPage/PurchaseCard.tsx
+++ components/bookPage/PurchaseCard.tsx
@@ -10,38 +10,45 @@ type Props = {
 
 const PurchaseCard = ({ book }: Props) => {
   const router = useRouter();
+  const { isFree, price } = { ...book };
   return (
-    <div className="flex flex-col gap-2">
-      <div className="card bg-primary text-primary-content h-fit w-80 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">
-            Free! <s>$5.99</s>
-          </h2>
-          <p className="text-sm">Available for a limited time</p>
-          <div className="card-actions justify-end mt-2">
-            <button className="btn btn-secondary btn-wide font-bold">
-              Add to my collection
-            </button>
+    <div className="flex flex-col gap-2 h-[568px]">
+      {isFree ? (
+        <div className="card bg-primary text-primary-content h-fit w-80 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">
+              Free! <s>${price}</s>
+            </h2>
+            <p className="text-sm">Available for a limited time</p>
+            <div className="card-actions justify-end mt-2">
+              <button className="btn btn-secondary btn-wide font-bold">
+                Add to my collection
+              </button>
 
-            <button
-              onClick={() => router.push(`${book._id}/read`)}
-              className="btn btn-primary shadow-none btn-wide font-bold"
-            >
-              Start reading
-            </button>
+              <button
+                onClick={() => router.push(`${book._id}/read`)}
+                className="btn btn-primary shadow-none btn-wide font-bold"
+              >
+                Start reading
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="card h-fit bg-primary text-primary-content w-80 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">
-            Only for <span className="text-green-500">$5.99</span>{" "}
-          </h2>
-          <div className="card-actions justify-end mt-3">
-            <button className="btn btn-secondary btn-wide">Add to cart</button>
+      ) : (
+        <div className="card h-fit bg-primary text-primary-content w-80 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">
+              Only for <span className="text-green-500">${price}</span>{" "}
+            </h2>
+            <div className="card-actions justify-end mt-3">
+              <button className="btn btn-secondary btn-wide">
+                Add to cart
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
+
       <ShareButtons book={book} />
     </div>
   );
